5ca0a6436a746948b505e4b71bb04d461bc3a2df
elooly10 <56009637+elooly10@users.noreply.github.com>
2020-07-29 12:33:25 -0600

    Create Pallate.js

---
+++ Pallate.js
@@ -0,0 +1,31 @@
+function Pallate(o, h, z) {
+  o += "";
+  h += "";
+  z += "";
+  this.Red = "#" + o + z + z;
+  this.DarkRed = "#" + h + z + z;
+  this.Pink = "#" + o + h + h;
+  this.Orange = "#" + o + h + z;
+  this.DarkYellow = "#" + h + h + z;
+  this.DarkPurple = "#" + h + z + h;
+  this.Ruby = "#" + o + z + h;
+  this.Yellow = "#" + o + o + z;
+  this.Laser = "#" + o + o + h;
+  this.Lime = "#" + z + o + z;
+  this.Green = "#" + z + h + z;
+  this.Key = "#" + h + o + h;
+  this.BrightMint = "#" + z + o + h;
+  this.DarkLime = "#" + h + o + z;
+  this.Teal = "#" + z + h + h;
+  this.Cyan = "#" + z + o + o;
+  this.BrightCyan = "#" + h + o + o;
+  this.Blue = "#" + z + z + o;
+  this.DarkBlue = "#" + z + z + h;
+  this.Purple = "#" + h + z + o;
+  this.Lavendar = "#" + h + h + o;
+  this.Denim = "#" + z + h + o;
+  this.Black = "#" + z + z + z;
+  this.White = "#" + o + o + o;
+  this.Gray = "#" + h + h + h;
+}
+// Yeah
